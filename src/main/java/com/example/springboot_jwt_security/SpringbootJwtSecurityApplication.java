dd8975785d7e5d28930fa7770c2ad3f2ed21ae88
Nabin Mahato <nabin@quantumaeon.com>
2025-11-11 11:08:39 +0530

    TEST Project

---
+++ src/main/java/com/example/springboot_jwt_security/SpringbootJwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jwt_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_jwt_security/SpringbootJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jwt_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
